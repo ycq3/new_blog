658ad1dd1b0fe28d03b77359fb96d6c0c3c58841
皮皮强 <pipiqiang@pipiqiang.com>
2022-11-28 08:43:06 +0800

    同步代码

--- src/pages/article/[id]/_compoment/artical_menu.tsx
+++ src/pages/article/[id]/_compoment/artical_menu.tsx
@@ -1,19 +1,36 @@
 import { Tree } from 'antd';
 import React from 'react';
 import { Anchor } from 'antd';
+import { Menu } from '../detail';
 const { Link } = Anchor;
 
-export default class ArticleMenu extends React.Component<any> {
+export default class ArticleMenu extends React.Component<any, any> {
   render(): React.ReactNode {
+    // console.log(this.props);debugger
+    console.log();
+
+    const { menus } = this.props;
     return (
       <>
-        <Anchor>
-          <Link href="#components-anchor-demo-basic" title="Basic demo" />
-          <Link href="#components-anchor-demo-static" title="Static demo" />
-          <Link href="#API" title="API">
-            <Link href="#Anchor-Props" title="Anchor Props" />
-            <Link href="#Link-Props" title="Link Props" />
-          </Link>
+        <Anchor onChange={(s) => console.log(s)}>
+          {menus.map((m: Menu) => {
+            console.log(m);
+            return (
+              <Link
+                href={location.href + '?_to=' + m.name}
+                title={m.name}
+                key={m.name}
+              >
+                {' '}
+              </Link>
+            );
+          })}
+          {/*<Link href="#components-anchor-demo-basic" title="Basic demo" />*/}
+          {/*<Link href="#components-anchor-demo-static" title="Static demo" />*/}
+          {/*<Link href="#API" title="API">*/}
+          {/*  <Link href="#Anchor-Props" title="Anchor Props" />*/}
+          {/*  <Link href="#Link-Props" title="Link Props" />*/}
+          {/*</Link>*/}
         </Anchor>
       </>
     );
--- src/pages/article/[id]/detail.tsx
+++ src/pages/article/[id]/detail.tsx
@@ -2,17 +2,21 @@ import React, { ReactNode } from 'react';
 import { Row, Col } from 'antd';
 import style from './detail.less';
 import { request } from 'umi';
-import MarkdownPreview from '@uiw/react-markdown-preview';
-import MarkdownNavbar from 'markdown-navbar';
-// import { MarkdownNavbar as MarkdownNavbarClass } from 'markdown-navbar';
-import ArticleMenu from './_compoment/artical_menu';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import ArticleMenu from '@/pages/article/[id]/_compoment/artical_menu';
+// import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import dark from 'react-syntax-highlighter/dist/esm/styles/prism/atom-dark';
 
-class Test extends MarkdownNavbar<any> {
-  //TS2507: Type '(options: MarkdownNavbarProps) => Element' is not a constructor function type.
-  updateHash() {
-    console.log('debug');
-    debugger;
-  }
+import { Anchor } from 'antd';
+
+const { Link } = Anchor;
+
+export interface Menu {
+  level: number;
+  name: string;
 }
 
 export default class ArticleDetail extends React.Component<any> {
@@ -24,6 +28,8 @@ export default class ArticleDetail extends React.Component<any> {
 
   state = {
     data: null,
+    content: <></>,
+    menus: [],
   };
 
   componentDidMount() {
@@ -33,7 +39,64 @@ export default class ArticleDetail extends React.Component<any> {
   loadDetail() {
     const { id } = this.props.match.params;
     request(`/api/article/${id}/detail`).then((resp) => {
-      this.setState({ data: resp.data });
+      this.setState({ data: resp.data }, () => {
+        this.renderBody();
+      });
+    });
+  }
+
+  renderBody() {
+    const {
+      data: { content },
+    } = this.state;
+
+    let menus: Menu[] = [];
+
+    let md = (
+      <ReactMarkdown
+        children={content}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          h2: ({ node, ...props }) => {
+            const { level, children } = props;
+            let p = children.pop();
+            if (p !== undefined && typeof p === 'string') {
+              let item = { level: level, name: p };
+              menus.push(item);
+              return <h2 id={item.name}>{item.name}</h2>;
+            } else {
+              console.log(p);
+            }
+            return <></>;
+          },
+          code({ node, inline, className, children, ...props }) {
+            // if(inline){
+            //   return;
+            // }
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    );
+    // React.createElement()
+
+    // ReactDOM.(md)
+    this.setState({ content: md }, () => {
+      this.setState({ menus: menus });
     });
   }
 
@@ -42,53 +105,28 @@ export default class ArticleDetail extends React.Component<any> {
       return <></>;
     }
 
-    // const syntaxTheme = oneDark;
-
-    // const MarkdownComponents: object = {
-    // code({ node, inline, className, ...props }) {}
-    // }
-
-    //   },
-
-    // const renderers = {
-    //   code: CodeBlock,
-    //   heading: HeadingBlock
-    // }
-
     const {
-      data: { title, html, content },
+      data: { title },
+      menus,
+      content,
     } = this.state;
 
     return (
       <>
         <div className={style.menu}>
-          {/*<ArticleMenu></ArticleMenu>*/}
-          <Test
-            source={content}
-            updateHashAuto={false}
-            ordered={false}
-            declarative={true}
-          />
+          <ArticleMenu menus={menus}></ArticleMenu>
+          {/*<Test*/}
+          {/*  source={content}*/}
+          {/*  updateHashAuto={false}*/}
+          {/*  ordered={false}*/}
+          {/*  declarative={true}*/}
+          {/*/>*/}
         </div>
         <Row>
           <Col offset={4} span={20}>
             <div className={style.center}>
               <div className={style.title}>{title}</div>
-              {/*<div dangerouslySetInnerHTML={{__html: html}}></div>*/}
-              {/*<ReactMarkdown*/}
-              {/*  // rehypePlugins={[rehypeHighlight]}*/}
-              {/*  rehypePlugins={[*/}
-              {/*    rehypeHighlight,*/}
-              {/*    rehypeRaw,*/}
-              {/*  ]}*/}
-              {/*  remarkPlugins={[remarkGfm]}*/}
-              {/*  // renderers={renderers}*/}
-              {/*>*/}
-              {/*  {content}*/}
-              {/*</ReactMarkdown>*/}
-              {/*{content}*/}
-              <MarkdownPreview source={content} />
-              {/*<div dangerouslySetInnerHTML={{__html: content}}></div>*/}
+              {content}
             </div>
           </Col>
         </Row>
